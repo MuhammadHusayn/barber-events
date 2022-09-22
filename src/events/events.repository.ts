244d74db93c2e2a7126931c926ee5644bc11c6ee
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2022-09-22 19:57:21 +0500

    update seed and repo

--- src/events/events.repository.ts
+++ src/events/events.repository.ts
@@ -1,39 +1,35 @@
 import { CreateBookingDto } from './dtos/create-booking.dto';
 import { BreakTime } from './entities/breakTime.entity';
 import { Booking } from './entities/booking.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 import { DayOff } from './entities/dayOff.entity';
 import { Event } from './entities/event.entity';
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class EventsRepository {
-    constructor(
-        @InjectRepository(BreakTime) private _breakTimesRepo: Repository<BreakTime>,
-        @InjectRepository(Booking) private _bookingsRepo: Repository<Booking>,
-        @InjectRepository(DayOff) private _dayOffsRepo: Repository<DayOff>,
-        @InjectRepository(Event) private _eventsRepo: Repository<Event>,
-    ) {}
+    constructor(private globalRepo: EntityManager) {}
 
     getEvents(): Promise<Event[]> {
-        return this._eventsRepo.find();
+        return this.globalRepo.getRepository(Event).find();
     }
 
     getDayOffs(): Promise<DayOff[]> {
-        return this._dayOffsRepo.find();
+        return this.globalRepo.getRepository(DayOff).find();
     }
 
     getBreakTimes(): Promise<BreakTime[]> {
-        return this._breakTimesRepo.find();
+        return this.globalRepo.getRepository(BreakTime).find();
     }
 
     getSlotBookingsCount(eventId: number, selectedDateTime: string): Promise<Booking[]> {
-        return this._bookingsRepo.find({ where: { eventId, selectedDateTime } });
+        return this.globalRepo
+            .getRepository(Booking)
+            .find({ where: { eventId, selectedDateTime } });
     }
 
     addBooking(bookingData: CreateBookingDto): Promise<Booking> {
-        const booking = this._bookingsRepo.create(bookingData);
-        return this._bookingsRepo.save(booking);
+        const booking = this.globalRepo.getRepository(Booking).create(bookingData);
+        return this.globalRepo.save(booking);
     }
 }
